c70542688d317d7bb751ba19978a1733783e8249
Andrew Dryga <andrew@dryga.com>
2017-04-15 16:28:11 +0300

    Removed trailing slack

--- app/common/config.js
+++ app/common/config.js
@@ -9,7 +9,7 @@ if (global.__CLIENT__ && window && window.__CONFIG__) {
 
 export const PORT = config.PORT || process.env.PORT || 8080;
 export const HOSTNAME = typeof window !== 'undefined' ? window.location.origin : (config.HOSTNAME || 'http://localhost:8080');
-export const API_HOST = config.API_HOST || process.env.API_HOST || 'https://man-api.herokuapp.com/';
+export const API_HOST = config.API_HOST || process.env.API_HOST || 'https://man-api.herokuapp.com';
 
 export const SITEMAP_HOSTNAME = process.env.SITEMAP_HOSTNAME || 'http://localhost:8080'; // used in sitemap
 export const LANG_COOKIE_NAME = 'lang';
