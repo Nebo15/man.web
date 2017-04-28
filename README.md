bd076452538f67c76254d5a244677e07eff9a3b0
Alexey Bondarenko <alexeybondarenko@me.com>
2017-04-28 18:48:14 +0300

    Rename API_HOST to API_ENDPOINT

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ Application supports these environment variables:
 | Environment Variable  | Default Value           | Description |
 | --------------------- | ----------------------- | ----------- |
 | `PORT`                | `8080`                  | Node.js server port. |
-| `API_HOST`            | `https://man-api.herokuapp.com` | Mán API endpoint. |
+| `API_ENDPOINT`            | `https://man-api.herokuapp.com` | Mán API endpoint. |
 | `SITEMAP_HOSTNAME`    | `http://localhost:8080` | URL will be used in sitemap generated urls |
 | `LANG_COOKIE_NAME`    | `lang`                  | Name of the cookie, where storing language variable |
 
--- app/common/config.js
+++ app/common/config.js
@@ -9,7 +9,7 @@ if (__CLIENT__ && window && window.__CONFIG__) {
 
 export const PORT = config.PORT || process.env.PORT || 8080;
 export const HOSTNAME = typeof window !== 'undefined' ? window.location.origin : (config.HOSTNAME || 'http://localhost:8080');
-export const API_HOST = config.API_HOST || process.env.API_HOST || 'https://man-api.herokuapp.com';
+export const API_ENDPOINT = config.API_ENDPOINT || process.env.API_ENDPOINT || 'https://man-api.herokuapp.com';
 
 export const SITEMAP_HOSTNAME = config.SITEMAP_HOSTNAME || process.env.SITEMAP_HOSTNAME || 'http://localhost:8080'; // used in sitemap
 export const LANG_COOKIE_NAME = config.LANG_COOKIE_NAME || process.env.LANG_COOKIE_NAME || 'lang';
@@ -17,4 +17,4 @@ export const LANG_COOKIE_NAME = config.LANG_COOKIE_NAME || process.env.LANG_COOK
 export const API_PROXY_PATH = '/api';
 
 // for internal app usage. for example for XHR requests or server side rendering
-export const API_URL = typeof window !== 'undefined' ? API_PROXY_PATH : API_HOST;
+export const API_URL = typeof window !== 'undefined' ? API_PROXY_PATH : API_ENDPOINT;
